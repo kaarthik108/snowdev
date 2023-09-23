108ed8d3e0a642d488963f6fb2884d2174fd350e
kaarthik108 <kaarthikandavar@gmail.com>
2023-09-24 12:32:56 +1300

    Test

---
+++ src/sproc/test_new_/app.py
@@ -0,0 +1,13 @@
+from snowflake.snowpark import Session
+
+def handler(session: Session, args) -> str:
+    customer_data = session.sql("SELECT * FROM customer_table LIMIT 1")
+    return customer_data.to_pandas()
+
+# Local testing
+# snowdev test --sproc sproc_name
+if __name__ == "__main__":
+    from snowdev import SnowflakeConnection
+    session = SnowflakeConnection().get_session()
+
+    print(handler(session,"test"))
---
+++ src/task/predict_task/app.sql
@@ -0,0 +1,6 @@
+CREATE OR REPLACE TASK predict_task
+  WAREHOUSE = COMPUTE_WH
+  SCHEDULE = 'USING CRON 0 1 * * * UTC'
+  COMMENT = 'This task runs daily at 1am UTC'
+AS
+    call test_script(); -- call stored procedure
