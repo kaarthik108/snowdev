79fe84984a07c8c6e9c6660d6f59541437e5d430
kaarthik108 <kaarthikandavar@gmail.com>
2023-09-24 15:13:48 +1300

    Apply .gitignore

--- src/task/predict_task/app.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE TASK predict_task
-  WAREHOUSE = COMPUTE_WH
-  SCHEDULE = 'USING CRON 0 1 * * * UTC'
-  COMMENT = 'This task runs daily at 1am UTC'
-AS
-    call test_script(); -- call stored procedure
-    
